492407e03cbfd897f8a7c7f1813a09ee5ff7ffb3
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-10 15:51:08 +0600

    bug fixed from createCard function

--- script.js
+++ script.js
@@ -92,7 +92,15 @@ const createCard = (book) => {
   const div = document.createElement("div");
   div.classList.add("card");
 
-  let overview = book.overview;
+  // let overview = book.overview;
+  let overview;
+
+  if(book.overview.length > 100 ){
+    console.log("beshi ace")
+    overview = book.overview.slice(0, 100)+ "...";
+  }else{
+    overview = book.overview;
+  }
 
   div.innerHTML = `
   <div class="image-container">
